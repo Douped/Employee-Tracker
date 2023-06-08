e34c29c45c839b6ad06b7ca6d5e616a3a536d46a
Douped <anthony.rooyakkers2001@gmail.com>
2023-06-08 11:10:38 -0400

    added functions to add departments, roles, and employees. Added function to update an existing employees role.

--- db/seed.sql
+++ db/seed.sql
@@ -7,12 +7,14 @@ VALUES ( "Science"),
        ("Jello Studies");
        
 INSERT INTO employee_roles (id, title, salary, department_id)
-VALUES (1, "CEO", 330000.00, 5),
- (2, "Minister of jello", 500000.00, 6),
- (3, "The Science Guy", 1500000.00, 1),
- (4, "Senior Managing Manager", 33433.00, 2),
- (5, "Junior jello trainee", 3.00, 6),
- (6, "Coffee getter", 0.02, 4);
+VALUES 
+ (1, "CEO", 330000.00, 5),
+ (2, "Intern", 0.00, 6),
+ (3, "Minister of jello", 500000.00, 6),
+ (4, "The Science Guy", 1500000.00, 1),
+ (5, "Senior Managing Manager", 33433.00, 2),
+ (6, "Junior jello trainee", 3.00, 6),
+ (7, "Coffee getter", 0.02, 4);
 
  INSERT INTO employees(first_name, last_name, role_id)
  VALUES("anthony","rooyakkers",1),("bill","nye",2);
--- index.js
+++ index.js
@@ -11,18 +11,61 @@ const db = mysql.createConnection(
     }
 );
 //Prompts
-const prompts = [{
+const prompts = [
+{
     type: 'list',
     name: 'main',
     message: 'What would you like to do? (use arrow keys)',
     choices: ["View All Departments", "View All Roles",
         "View All Employees", "Add A Department", "Add A Role",
         "Add An Employee", "Update An Employee Role", "Exit"],
-}, {
+},
+{
     type: 'input',
     name: 'department',
     message: 'Enter a department name: ',
-}];
+}, 
+{
+    type: 'input',
+    name: 'role',
+    message: 'Enter a role name: ',
+},
+{
+    type: 'input',
+    name: 'salary',
+    message: 'Enter a salary for the role: ',
+},
+{
+    type: 'input',
+    name: 'department_id',
+    message: 'Enter the department ID to add the role to: ',
+},
+{
+    type: 'input',
+    name: 'first_name',
+    message: 'Enter the employees first name: ',
+},
+{
+    type: 'input',
+    name: 'last_name',
+    message: 'Enter the employees last name: ',
+},
+{
+    type: 'input',
+    name: 'role_id',
+    message: 'Enter the ID of the role to assign the employee to (leave empty if unknown): ',
+},
+{
+    type: 'input',
+    name: 'manager_id',
+    message: 'Enter the ID of the manager to assign to the employee (leave empty if unknown): ',
+},
+{
+    type: 'input',
+    name: 'employee_id',
+    message: 'Enter the ID of the employee whose role you would like to modify: ',
+},
+];
 //View all departments
 viewAllDepartments = () => {
     db.query('SELECT d.id AS ID, d.department_name AS Department FROM departments d', (err, result) => {
@@ -30,6 +73,7 @@ viewAllDepartments = () => {
             console.log(err);
         } else {
             console.log(result);
+            mainPrompt();
         }
     });
 }
@@ -41,6 +85,7 @@ viewAllRoles = () => {
                 console.log(err);
             } else {
                 console.log(result);
+                mainPrompt();
             }
         });
 }
@@ -52,6 +97,7 @@ viewAllEmployees = () => {
                 console.log(err);
             } else {
                 console.log(result);
+                mainPrompt();
             }
         });
 }
@@ -60,45 +106,67 @@ addDepartment = () => {
     inquirer
     .prompt([prompts[1]])
     .then((data) =>{
-        db.query(`INSERT INTO departments (department_name) VALUES (${data.department})`, (err, result) => {
+        db.query('INSERT INTO departments (department_name) VALUES (?)', data.department, (err, result) => {
             if (err) {
                 console.log(err);
             } else {
-                console.log(result);
+                console.log(`Added ${data.department} to departments list.`);
+                mainPrompt();
             }
         });
     })
-    
 }
 //Add a role
 addRole = () => {
-    db.query('', (err, result) => {
-        if (err) {
-            console.log(err);
-        } else {
-            console.log(result);
-        }
-    });
+    inquirer
+    .prompt([prompts[2], prompts[3], prompts[4]])
+    .then((data) =>{
+        db.query('INSERT INTO employee_roles (title, salary, department_id) VALUES (?, ?, ?)', [data.role, data.salary, data.department_id], (err, result) => {
+            if (err) {
+                console.log(err);
+            } else {
+                console.log(`Added ${data.role} to roles list.`);
+                mainPrompt();
+            }
+        });
+    })
 }
 //Add an employee
 addEmployee = () => {
-    db.query('', (err, result) => {
-        if (err) {
-            console.log(err);
-        } else {
-            console.log(result);
+    inquirer
+    .prompt([prompts[5], prompts[6], prompts[7], prompts[8]])
+    .then((data) =>{
+        if(data.role_id == ''){
+            //if no role is specified, set the role to intern
+            data.role_id = 2;
         }
-    });
+        if(data.manager_id == ''){
+            data.manager_id = null;
+        }
+        db.query('INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)', [data.first_name, data.last_name, data.role_id, data.manager_id], (err, result) => {
+            if (err) {
+                console.log(err);
+            } else {
+                console.log(`Added ${data.first_name} to employees list.`);
+                mainPrompt();
+            }
+        });
+    })
 }
-//Update an employee role
+//Update an employees role
 updateEmployeeRole = () => {
-    db.query('', (err, result) => {
-        if (err) {
-            console.log(err);
-        } else {
-            console.log(result);
-        }
-    });
+    inquirer
+    .prompt([prompts[9], prompts[7]])
+    .then((data) =>{
+        db.query('UPDATE employees SET role_id = ? WHERE id = ?', [data.role_id, data.employee_id], (err, result) => {
+            if (err) {
+                console.log(err);
+            } else {
+                console.log(`Updated employees role.`);
+                mainPrompt();
+            }
+        });
+    })
 }
 //Start prompt
 mainPrompt = async () => {
@@ -107,36 +175,35 @@ mainPrompt = async () => {
             prompts[0],
         ])
         .then((data) => {
-            console.log(data);
             try {
                 switch (data.main) {
                     case "View All Departments":
                         viewAllDepartments();
-                        mainPrompt();
+                        
                         break;
                     case "View All Roles":
                         viewAllRoles();
-                        mainPrompt();
+                        
                         break;
                     case "View All Employees":
                         viewAllEmployees();
-                        mainPrompt();
+                        
                         break;
                     case "Add A Department":
                         addDepartment();
-                        //mainPrompt();
+                        
                         break;
                     case "Add A Role":
                         addRole();
-                        mainPrompt();
+                        
                         break;
                     case "Add An Employee":
                         addEmployee();
-                        mainPrompt();
+                        
                         break;
                     case "Update An Employee Role":
                         updateEmployeeRole();
-                        mainPrompt();
+                        
                         break;
                     case "Exit":
                         break;
